7da4d6f64c1cbfb17e3533ff607f5b5e4ccb76f6
Arpan B <arpanbhalla@gmail.com>
2016-09-30 08:54:31 +1000

    More Boot Strap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  
-  root "articles#index"
+
+  get 'pages/about'
+
+  root 'pages#home'
+
+
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
